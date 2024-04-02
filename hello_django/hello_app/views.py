70d64be49936ba4c5130a5c77b6091d58a03e414
Aswathi M <aswathim2402@gmail.com>
2024-04-02 15:16:18 +0530

    Simple html page created with django.

--- hello_django/hello_app/views.py
+++ hello_django/hello_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def print_hello(request):
-    return HttpResponse('hello')
+    return render(request,'hello.html')

d4016521638362ea6418385560f2f0698697c7c0
Aswathi M <aswathim2402@gmail.com>
2024-04-02 15:42:52 +0530

    Movie list created in a web page using a dictionary.

--- hello_django/hello_app/views.py
+++ hello_django/hello_app/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def print_hello(request):
-    return render(request,'hello.html')
+    movie_list={'title':'Aadujeevitham',
+                'year':2024,
+                'Summary':'Survival thriller'}
+    return render(request,'hello.html',movie_list)

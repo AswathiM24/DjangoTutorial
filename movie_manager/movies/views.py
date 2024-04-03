7e4504d09a249e886b4858f83e8d5486abe2aa34
Aswathi M <aswathim2402@gmail.com>
2024-04-03 11:51:39 +0530

    Added images for managing static files,created a form.

--- movie_manager/movies/views.py
+++ movie_manager/movies/views.py
@@ -2,10 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 def create(request):
+    if request.POST:
+        print(request.POST.get('title'))
+        print(request.POST.get('year'))
     return render(request,'create.html')
 
 def list(request):
-    return render(request,'list.html')
+    movie_data={'movies':[{'title':'Aadujeevitham',
+                'year':2024,
+                'success':False,
+                'Summary':'Survival thriller',
+                'img':'goat.jpeg'},
+                {'title':'Manjummal boys',
+                'year':2024,
+                'Summary':'Survival thriller',
+                'success':True,
+                'img':'manjummel.jpeg'}
+                ]}
+    return render(request,'list.html',movie_data) 
 
 def edit(request):
     return render(request,'edit.html')

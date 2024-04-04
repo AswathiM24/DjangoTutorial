ba10c46fd985acfdcbc79a02a3596eca54ed7b0e
Aswathi M <aswathim2402@gmail.com>
2024-04-04 09:36:57 +0530

    Creataed a migration file

--- movie_manager/movies/models.py
+++ movie_manager/movies/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class movieInfo(models.Model):
+    title=models.CharField(max_length=250)
+    year=models.IntegerField(null=True)
+    description=models.TextField()
+    
+

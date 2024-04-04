5da81ebd27bcce6dbfd6ae775c5d8b708ea843c3
Aswathi M <aswathim2402@gmail.com>
2024-04-04 16:01:43 +0530

    Model created,table created.retrieved data from the model and printed it.

--- movie_manager/movies/models.py
+++ movie_manager/movies/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
 class movieInfo(models.Model):
     title=models.CharField(max_length=250)
     year=models.IntegerField(null=True)
     description=models.TextField()
+
+class Director(models.Model):
+    name=models.CharField(max_length=300)
+    
     
 
--- movie_manager/movies/views.py
+++ movie_manager/movies/views.py
@@ -1,25 +1,31 @@
 from django.shortcuts import render
+from . models import movieInfo
 
 # Create your views here.
 def create(request):
     if request.POST:
-        print(request.POST.get('title'))
-        print(request.POST.get('year'))
+        title=request.POST.get('title')
+        year=request.POST.get('year')
+        desc=request.POST.get('summary')
+        movie_obj=movieInfo(title=title,year=year,description=desc)
+        movie_obj.save()
     return render(request,'create.html')
 
 def list(request):
-    movie_data={'movies':[{'title':'Aadujeevitham',
-                'year':2024,
-                'success':False,
-                'Summary':'Survival thriller',
-                'img':'goat.jpeg'},
-                {'title':'Manjummal boys',
-                'year':2024,
-                'Summary':'Survival thriller',
-                'success':True,
-                'img':'manjummel.jpeg'}
-                ]}
-    return render(request,'list.html',movie_data) 
+    # movie_data={'movies':[{'title':'Aadujeevitham',
+    #             'year':2024,
+    #             'success':False,
+    #             'Summary':'Survival thriller',
+    #             'img':'goat.jpeg'},
+    #             {'title':'Manjummal boys',
+    #             'year':2024,
+    #             'Summary':'Survival thriller',
+    #             'success':True,
+    #             'img':'manjummel.jpeg'}
+    #             ]}
+    movie_set=movieInfo.objects.all()
+    print(movie_set)
+    return render(request,'list.html',{'movies':movie_set}) 
 
 def edit(request):
     return render(request,'edit.html')
